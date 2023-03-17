0a92cf95acdaf5571cffa72080bd91c708d23220
Don-001 <claudiodex009@gmail.com>
2023-03-17 12:50:11 +0300

    Add integrity validators for the email

--- website/auth.py
+++ website/auth.py
@@ -8,10 +8,23 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email= request.form.get('email')
+        password = request.form.get('password')
+        
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in Successfully!', category='success')
+            else:
+                flash('Incrrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
+    
     return "<p>logout<p>"
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -22,7 +35,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if password1 != password2:
+        user=User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        
+        elif password1 != password2:
             flash('Passwords do not match.', category='error')
         elif len(password1) < 8:
                 flash('Passwords must be at least 8 characters long.', category='error')
