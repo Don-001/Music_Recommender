89865f3d858e711e8bc8fceefe3db4a770941f1b
Don-001 <claudiodex009@gmail.com>
2023-03-25 13:35:51 +0300

    Do away with some code clutter

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ from website import create_app
 
 app = create_app()
 
-#@app.route('/')   # URL '/' to be handled by main() route handler (or view function)
-#def login():
-   # if request.method == 'GET':
-        # render the login form template
-   #     return render_template('login.html')
-   # elif request.method == 'POST':
-   #     username = request.form['username']
-   #     password = request.form['password']
-    #    if not validate_login(username, password):
-            # if the login is invalid, re-render the form with an error message
-     #       return render_template('login.html', error='Invalid username or password')
-     #   else:
-            # continue with login process
-            # return a success message or redirect to a different page
-       #     return "Login successful"
-
 
 #def register():
    # if request.method == 'GET':
--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
+
+
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods= ['POST', 'GET'])
 @login_required
 def search():
-    return render_template("search.html", user=current_user)
+   return render_template("search.html", user=current_user)
