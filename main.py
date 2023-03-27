8ae1ad60658e9be93db4f24163f487e17e41717f
Don-001 <claudiodex009@gmail.com>
2023-03-27 13:07:26 +0300

    Clean up code clutter

--- main.py
+++ main.py
@@ -7,19 +7,7 @@ from website import create_app
 app = create_app()
 
 
-#def register():
-   # if request.method == 'GET':
-        # render the registration form template
-     #   return render_template('register.html')
-   # elif request.method == 'POST':
-    #    email = request.form['email']
-       # if not validate_email(email):
-            # if the email is invalid, re-render the form with an error message
-      #      return render_template('register.html', error='Invalid email address')
-   # else:
-            # continue with registration process
-            # return a success message or redirect to a different page
-      #  return "Registration successful"
+
 
 if __name__ == '__main__':  # Script executed directly (instead of via import)?
         app.run(debug=True)  # Launch built-in web server and run this Flask webapp
--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 
-@views.route('/', methods= ['POST', 'GET'])
+@views.route('/')
 @login_required
 def search():
    return render_template("search.html", user=current_user)
